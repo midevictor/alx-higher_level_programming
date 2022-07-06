d92db232f98786c8fe2c92120b27c526b32be9ea
Ayomide Victor <midevictor90@gmail.com>
2022-07-06 20:31:59 +0100

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for lookup task
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods
+    of an object
+    """
+    return dir(obj)
