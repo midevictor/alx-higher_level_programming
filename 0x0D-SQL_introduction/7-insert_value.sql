e76306488c0342efa5b2464fd997074da4488312
Ayomide Victor <midevictor90@gmail.com>
2022-08-11 17:50:53 +0100

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---Insert into
-INSERT INTO hbtn_0c_0.first_table(id, name) VALUES (89, "Best School");
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
